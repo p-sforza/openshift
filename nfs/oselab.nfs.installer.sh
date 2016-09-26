e4c64397c6c4176483d1aa25034f425e5c92759e
p-sforza <psforza@redhat.com>
2016-09-26 20:23:21 +0200

    main rework

--- nfs/oselab.nfs.installer.sh
+++ nfs/oselab.nfs.installer.sh
@@ -1,54 +1,29 @@
 #!/bin/bash
 
-echo "Setting up disk..."
-parted -s /dev/vdb mklabel msdos
-parted -s /dev/vdb mkpart primary ext4 0 100%
-mkfs.ext4 /dev/vdb1
-mkdir /srv/nfs
+# Configure your environment
+export NFS_DISK=/dev/vdb
+export NFS_PARTITION=/dev/vdb1
+export NFS_VG_NAME=vg-nfs
+export NFS_LV_NAME=lv-nfs
+export NFS_MNT_POINT=/var/nfs
 
-grep -q "/dev/vdb1" /etc/fstab
-if [ $? -eq 1 ]
-then
-  echo "/dev/vdb1    /srv/nfs ext4    defaults    0    0" >> /etc/fstab
-fi
-
-mount /dev/vdb1 /srv/nfs
-mkdir -p /srv/nfs/{registry,vol1,vol2,vol3,es-storage}
-chmod -R 777 /srv/nfs/*
-chown -R nfsnobody:nfsnobody /srv/nfs
-
-## We need to update /etc/fstab in this script <sborenst>
-
-echo "Setting up exports..."
-cat << EOF > /etc/exports
-/srv/nfs/registry *(rw,root_squash)
-/srv/nfs/vol1 *(rw,root_squash)
-/srv/nfs/vol2 *(rw,root_squash)
-/srv/nfs/vol3 *(rw,root_squash)
-/srv/nfs/es-storage *(rw,root_squash)
-
+# Prepare the disk, assign it to LVM and create fs
+echo "--> Setting up disk..."
+sfdisk ${NFS_DISK} << EOF
+;
 EOF
 
-echo "Setting up firewall..."
-iptables -I INPUT -p tcp -m state --state NEW -m tcp --dport 111 -j ACCEPT
-iptables -I INPUT -p tcp -m state --state NEW -m tcp --dport 2049 -j ACCEPT
-iptables -I INPUT -p tcp -m state --state NEW -m tcp --dport 20048 -j ACCEPT
-iptables -I INPUT -p tcp -m state --state NEW -m tcp --dport 50825 -j ACCEPT
-iptables -I INPUT -p tcp -m state --state NEW -m tcp --dport 53248 -j ACCEPT
+pvcreate ${NFS_PARTITION}
+vgcreate ${NFS_VG_NAME} ${NFS_PARTITION}
+lvcreate -l 100%FREE -n ${NFS_LV_NAME} ${NFS_VG_NAME}
+mkfs.xfs -q -f /dev/${NFS_VG_NAME}/${NFS_LV_NAME}
+
+# Install and configure NFS
+echo "--> Installing NFS..."
+yum -y install nfs-utils rpcbind 
 
 echo "Configuring NFS..."
 sed -i -e 's/^RPCMOUNTDOPTS.*/RPCMOUNTDOPTS="-p 20048"/' -e 's/^STATDARG.*/STATDARG="-p 50825"/' /etc/sysconfig/nfs
-
-grep -q "dport 53248" /etc/sysconfig/iptables
-if [ $? -eq 1 ]
-then
-sed -i -e '/^:OUTPUT/a -I INPUT -p tcp -m state --state NEW -m tcp --dport 53248 -j ACCEPT\
--I INPUT -p tcp -m state --state NEW -m tcp --dport 50825 -j ACCEPT\
--I INPUT -p tcp -m state --state NEW -m tcp --dport 20048 -j ACCEPT\
--I INPUT -p tcp -m state --state NEW -m tcp --dport 2049 -j ACCEPT\
--I INPUT -p tcp -m state --state NEW -m tcp --dport 111 -j ACCEPT' \
-  /etc/sysconfig/iptables
-fi
 
 echo "Setting sysctl params..."
 grep -q fs.nlm /etc/sysctl.conf
@@ -57,12 +32,47 @@ then
   sed -i -e '$afs.nfs.nlm_tcpport=53248' -e '$afs.nfs.nlm_udpport=53248' /etc/sysctl.conf
 fi
 
-echo "Setting up services..."
+# Mount the Volume
+mount -a
+echo "--> Create local mounts..."
+mkdir -p ${NFS_MNT_POINT}/{registry,vol1,vol2,vol3,es-storage}
+chmod -R 777 ${NFS_MNT_POINT}/*
+chown -R nfsnobody:nfsnobody ${NFS_MNT_POINT}
+
+# Export the NFS partition 
+echo "--> Export mounts..."
+cat << EOF >> /etc/exports
+${NFS_MNT_POINT}/registry *(insecure,rw,root_squash)
+${NFS_MNT_POINT}/vol1 *(insecure,rw,root_squash)
+${NFS_MNT_POINT}/vol2 *(insecure,rw,root_squash)
+${NFS_MNT_POINT}/vol3 *(insecure,rw,root_squash)
+${NFS_MNT_POINT}/es-storage *(insecure,rw,root_squash)
+EOF
+
+grep -q "/dev/${NFS_VG_NAME}/${NFS_LV_NAME}" /etc/fstab
+if [ $? -eq 1 ]
+then
+cat << EOF >> /etc/fstab
+/dev/${NFS_VG_NAME}/${NFS_LV_NAME}        ${NFS_MNT_POINT}              xfs defaults 0 0
+EOF
+fi
+
+
+
+# Setup firewall
+echo "--> Setting up firewall..."
+iptables -I INPUT -p tcp -m state --state NEW -m tcp --dport 111 -j ACCEPT
+iptables -I INPUT -p tcp -m state --state NEW -m tcp --dport 2049 -j ACCEPT
+iptables -I INPUT -p tcp -m state --state NEW -m tcp --dport 20048 -j ACCEPT
+iptables -I INPUT -p tcp -m state --state NEW -m tcp --dport 50825 -j ACCEPT
+iptables -I INPUT -p tcp -m state --state NEW -m tcp --dport 53248 -j ACCEPT
+
+echo "--> Setting up services..."
 systemctl enable rpcbind nfs-server
 systemctl start rpcbind nfs-server nfs-lock
 systemctl start nfs-idmap
 sysctl -p
 systemctl restart nfs
 
-echo "Setting NFS seboolean..."
+echo "--> Setting NFS seboolean..."
 setsebool -P virt_use_nfs=true
